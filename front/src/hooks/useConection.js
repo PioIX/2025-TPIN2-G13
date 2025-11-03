b536d0d75b89ffbf5b51f17ed76d00fca5c7c37b
FranManzanares <fmanzanares@pioix.edu.ar>
2025-11-03 08:17:08 -0300

    fff

--- front/src/hooks/useConection.js
+++ front/src/hooks/useConection.js
@@ -1,5 +1,5 @@
  const useConection = () =>{
-    const ip = "http://10.1.5.88" 
+    const ip = "http://10.1.5.134" 
     const port = "4006"
 
     const url = ip + ":" + port
--- front/src/hooks/useSocket.js
+++ front/src/hooks/useSocket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 
 
 
-const useSocket = (options = { withCredentials: false }, serverUrl = "ws://10.1.5.88:4006/") => { //ACÁ PONER LA IP DEL BACK
+const useSocket = (options = { withCredentials: false }, serverUrl = "ws://10.1.5.134:4006/") => { //ACÁ PONER LA IP DEL BACK
   const [socket, setSocket] = useState(null);
   const [isConnected, setIsConnected] = useState(false) 
   
